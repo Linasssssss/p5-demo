32a783fa2d99caad09f125ac2fdd80259cc1d6f8
Linasssssss <linaszauras@gmail.com>
2026-04-18 11:13:40 +0300

    Create snake grid drawing function

--- sketch.js
+++ sketch.js
@@ -1,8 +1,12 @@
 var setupScript = function() {};
 var drawScript = function() {};
 
+var canvasX = 400;
+var canvasY = 400;
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasX, canvasY);
 
 
 }
